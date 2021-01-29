a2ae1cb680135e227caac44e5e68605acf3b2333
Mattes <mattes.voigts@stud.uni-goettingen.de>
2021-01-30 00:17:07 +0100

    test

---
+++ Assets/spin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    public GameObject obj;
+    private bool s;
+    int rotationCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!s)
+            rotationCounter = 0;
+        s = true;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (s && rotationCounter != 90)
+        {
+            obj.transform.Rotate(new Vector3(0f,0f,1f));
+            rotationCounter++;
+        }
+        else
+        {
+            s = false;
+        }
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public float rotaionSpeed;
+    public GameObject obj;
+    private bool s;
+    float rotationCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!s)
+                rotationCounter = 0;
+            s = true;
+        }
+
+        if (s && rotationCounter != 90/rotaionSpeed)
+        {
+            obj.transform.Rotate(new Vector3(0f, 0f, -rotaionSpeed));
+            rotationCounter += rotaionSpeed;
+        }
+        else
+        {
+            s = false;
+        }
+    }
+}
