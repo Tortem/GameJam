2f6c980aef26386f962261efe6562a6913e57d10
Torben <torbboettcher@outlook.de>
2021-01-30 13:18:30 +0100

    menu and sprint

--- Assets/Character/NonPhysicMovement.cs
+++ Assets/Character/NonPhysicMovement.cs
@@ -9,18 +9,27 @@ public class NonPhysicMovement : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity = Vector3.zero;
     [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float playerSprintSpeed = 4.0f;
     [SerializeField] private float jumpHeight = 1.0f;
     [SerializeField] private float gravityValue = -9.81f;
     [SerializeField] private bool isGrounded;
+    [SerializeField] public bool movementAllowed = false;
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        movementAllowed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!movementAllowed) { return; }
+
+        float speed;
+        if (Input.GetButton("Sprint")) { speed = playerSprintSpeed; }
+        else { speed = playerSpeed; }
+
         isGrounded = controller.isGrounded;
 
         // movement
@@ -28,7 +37,7 @@ public class NonPhysicMovement : MonoBehaviour
         float verticalMove = Input.GetAxis("Vertical");
 
         Vector3 move = transform.forward * verticalMove + transform.right * horizontalMove;
-        controller.Move(move * Time.deltaTime * playerSpeed + playerVelocity * Time.deltaTime);
+        controller.Move(move * Time.deltaTime *  speed + playerVelocity * Time.deltaTime);
 
         // jumping and gravity
         if (isGrounded && playerVelocity.y < 0)
