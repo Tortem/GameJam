5f4d7ef7ee7c6e88ed039637ef038828f1577229
Mattes <mattes.voigts@stud.uni-goettingen.de>
2021-01-30 15:39:47 +0100

    kein wim

---
+++ Assets/rotateZ.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateZ : MonoBehaviour
+{
+    public GameObject cubeAxis;
+    private void OnTriggerEnter(Collider other)
+    {
+        cubeAxis.GetComponent<spin>().rotateZ(gameObject);
+    }
+}
